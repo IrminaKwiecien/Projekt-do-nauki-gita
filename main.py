3c1644582fe2a598a13d8c2a8e23cb3b74cb1760
IrminaKwiecien <irmina.kwiecien@wp.pl>
2017-11-25 22:14:43 +0100

    koniec zajec

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
-imie = 'Irmina'
+def wypisz_samochod(marka, model):
+    print("{0},{1}".format(marka,model))
 
-print("Witaj w swiecie, {0}!".format(imie))
+def czystary(wiek):
+    MAX = 15
+    if wiek < MAX:
+        return False
+    else:
+        return True
+
+if __name__=="__main__":
+    marka = 'Fiat'
+    model = 'Punto'
+    wiek = 10
+
+    wypisz_samochod(marka, model)
+    if wiek < 15:
+        print('Nowe')
+    else:
+        print('Stare')
+
+
+
+
+#if_name_=="_main_":
+
+#imie = 'Irmina'
+#nazwisko = 'B'
+
+#pelne_imie = imie + '' + nazwisko
+
+#ilosc_zwierzat_domowych = 2
+#cena = 2 * 120
+
+#print(cena)
+#print(pelne_imie)
+
+
+
+#print("Witaj w swiecie, {0}!".format(imie))
